4a0f7104a83b5e7b8c87395008c7e699519bab1d
Kristen Robertson <kristen77@gmail.com>
2018-10-20 23:16:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
-   [:fourth_of_july].keys.min
+   :fourth_of_july.keys.min
   # return the second element in the 4th of July array
 end
 
