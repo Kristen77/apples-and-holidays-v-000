29089a639cac7bf53fe37d01747270b29f971432
Kristen Robertson <kristen77@gmail.com>
2018-10-20 23:58:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_supplies.each do |season, holidays_hash|
+    supplies_hash.each do |data|
 end
 
 def all_holidays_with_bbq(holiday_hash)
